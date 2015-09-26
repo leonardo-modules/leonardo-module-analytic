be916cf4373d6fb7c5b0e8f0723c7b26f01eb37f
Michael Kutý <6du1ro.n@gmail.com>
2015-09-26 12:23:01 +0200

    no activate analytics if is not set

--- leonardo_module_analytics/__init__.py
+++ leonardo_module_analytics/__init__.py
@@ -1,6 +1,8 @@
 
+from django.utils import six
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings as django_settings
 
 
 default_app_config = 'leonardo_module_analytics.AnalyticsConfig'
@@ -23,5 +25,16 @@ class Default(object):
 class AnalyticsConfig(AppConfig, Default):
     name = 'leonardo_module_analytics'
     verbose_name = ("Leonardo Analytics")
+
+    def ready(self):
+        # chech defaults and delete it from main settings
+        # because it's activate JS in templates
+        from constance import config
+
+        for k, values in six.iteritems(self.config):
+            if values[0] == getattr(config, k, None):
+
+                setattr(django_settings, k, None)
+
 
 default = Default()
